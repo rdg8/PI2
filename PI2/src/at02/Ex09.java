ad10906bf8732a2b647596f5156666af628f9418
rdg8 <digorinaldi@gmail.com>
2017-08-29 15:41:29 -0300

    Add files via upload

--- PI2/src/at02/Ex09.java
+++ PI2/src/at02/Ex09.java
@@ -8,12 +8,112 @@ package at02;
 
 import java.util.Scanner;
 
-
 public class Ex09 {
     static Scanner input= new Scanner(System.in);
     
     public static void main(String[] args) {
+        boolean sair=false;
+        String aulas[]=entrada();
+        aulas=aulas(aulas);
+        do{
+            System.out.println("Escolha um dia da semana \ndigite todas para ver todas as aulas\n ou digite sair:");
+            String dia=input.next();
+            sair=opcoes(dia, aulas);
+        }while(!sair);
+    }
+    
+    static String[] entrada(){
+        String aula[]= new String [14];
+        return aula;
+    }
+    
+    static boolean opcoes(String dia, String vetor[]){
+        switch(dia){
+            case "1":
+            case "domingo":
+                System.out.println("Aulas de Domingo");
+                for(int i=0;i<=1;i++){
+                    System.out.println(vetor[i]);
+                }
+                break;
+            case "2":
+            case "segunda":
+                System.out.println("Aulas de Segunda");
+                for(int i=2;i<=3;i++){
+                    System.out.println(vetor[i]);
+                }
+                break;
+            case "3":
+            case "terça":
+                System.out.println("Aulas de Domingo");
+                for(int i=4;i<=5;i++){
+                    System.out.println(vetor[i]);
+                }
+             break;
+            case "4":
+            case "quarta":  
+                System.out.println("Aulas de Domingo");
+                for(int i=6;i<=7;i++){
+                    System.out.println(vetor[i]);
+                }
+                break;
+            case "5":
+            case "quinta":
+                System.out.println("Aulas de Domingo");
+                for(int i=8;i<=9;i++){
+                    System.out.println(vetor[i]);
+                }
+                break;
+            case "6":
+            case "sexta":
+                System.out.println("Aulas de Domingo");
+                for(int i=10;i<=11;i++){
+                    System.out.println(vetor[i]);
+                }
+                break;
+            case "7":
+            case "sabado":
+                System.out.println("Aulas de Domingo");
+                for(int i=12;i<=13;i++){
+                    System.out.println(vetor[i]);
+                }
+                break;
+            case "0":
+            case "sair":
+               boolean sair=true;
+               return sair;  
+            case "todas":
+                System.out.println("todas as aulas da semna: ");
+                 imprimirVetor(vetor);
+                 break;
+            default:
+                System.out.println("ERRO digite um numero de 0 a 7 ou um dia da semana");
+        }
         
+        boolean sair=false;
+        return sair;
+        
+    }
+    
+    static String[]  aulas(String aulas[]){
+        int d=1;
+        for (int i = 0; i < aulas.length;i++){
+            System.out.println("Dia"+d+" da semana");
+            aulas[i]=input.next();    
+            if(i%2!=0 || i==1){
+                System.out.println("");
+                d++;
+            }
+        }
+   
+        return aulas;
+    }
+    
+    static void imprimirVetor(String vetor[]){
+        for (int i = 0; i < vetor.length; i++) {
+            System.out.println("\nposicao"+i+" "+vetor[i]);
+            
+        }
     }
     
 }
